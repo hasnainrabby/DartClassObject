c086b49ccd6e8736794dfe2465c51d37c81bb578
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-07-27 22:30:12 +0600

    Introduction to Constructor Dart: Syntactic Sugar Constructor ,Named Constructor

--- main.dart
+++ main.dart
@@ -9,6 +9,17 @@ class Car{                                                                   // 
  //Declaration of variables that will be initialize later is done using late modifier.
  // late is a modifier,  can be used while declaring a non-nullable variable that’s initialized after its declaration.
 
+  //Syntactic sugar constructor
+  Car(this.carName,this.brandName,this.model);
+
+  // this = keyword, means this object/class.
+  //Named Constructor
+  Car.newversioncar()
+  {
+    carName = "Jaguar Jeep";
+    model = 2345;
+ }
+
   //methods
  void carStart()
   {
@@ -26,12 +37,19 @@ class Car{                                                                   // 
 main(){
 
 
-  var car = new Car();                                              //creating actual object of type Car.
-  car.carName = "Sidan";
+  var car = new Car("Sidan","BMW",135480);                                              //creating actual object of type Car.
+
+  var jeep = new Car.newversioncar();                            //Using Named Constructor.
+  print(jeep.carName);
+  print(jeep.model);
+
+
+  /* car.carName = "Sidan";
   car.brandName = "BMW";
   car.color = "Black";
-  car.model = 135480;
+  car.model = 135480;*/
   print(car.brandName);
+  print(car.carName);
 
   car.carStart();                                                        //invoke methods,those are created on class field.
   car.carStop();
